a634e94cbfc9d5ea03454ce06158dc612f3676d7
manok584 <manohar.kamarushi@gmail.com>
2020-12-17 08:40:34 +0530

    StudentCRUDTest.java upadte,delete student validate

--- src/test/java/com/studentapp/junit/crudTests/StudentCRUDTest.java
+++ src/test/java/com/studentapp/junit/crudTests/StudentCRUDTest.java
@@ -1,8 +1,11 @@
 package com.studentapp.junit.crudTests;
 
+import static org.junit.Assert.assertThat;
+
 import java.util.ArrayList;
 import java.util.HashMap;
 
+import org.hamcrest.Matcher;
 import org.junit.FixMethodOrder;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -17,6 +20,8 @@ import net.serenitybdd.junit.runners.SerenityRunner;
 import net.serenitybdd.rest.SerenityRest;
 import net.thucydides.core.annotations.Title;
 
+import static org.hamcrest.Matchers.*;
+
 @RunWith(SerenityRunner.class)
 @FixMethodOrder(MethodSorters.NAME_ASCENDING)
 public class StudentCRUDTest extends TestBase {
@@ -27,6 +32,8 @@ public class StudentCRUDTest extends TestBase {
 	static String lastName = "lnameUser"+randomValue;
 	static String trainingProgramme = "foo";
 	static String emailID = randomValue+"fl@gmail.com";
+	
+	static String studentId;
 	
 	@Title("This test will create a new student")
 	@Test
@@ -91,15 +98,85 @@ public class StudentCRUDTest extends TestBase {
 		// it is a json path syntax from restAssured
 		
 		System.out.println("value extracted -"+value);
+		studentId = (String) value.get("id");
 		
+//		assertThat(value, hasvalue(firstName));
 	}
 	
 	
-	@Title("This test validates the extracted student record")
+
+	@Title("update the student info and validate info weather updated")
 	@Test
 	public void t3() {
 		
+		ArrayList<String> courses =new ArrayList<String>();
+		courses.add("Java");
+		courses.add("python");
 		
+		firstName = firstName+"_updated";
+		
+		StudentClass student = new StudentClass();
+		student.setFirstName(firstName);
+		student.setLastName(lastName);
+		student.setEmail(emailID);
+		student.setProgramme(trainingProgramme);
+		student.setCourses(courses);
+		
+			
+		SerenityRest.rest().given()
+		.contentType(ContentType.JSON)
+		.log()
+		.all()
+		.when()
+		.body(student)
+		.put("/"+studentId)
+		.then()
+		.log()
+		.all();
 	}
 
+	@Title("This test verifies the updated request")
+	@Test
+	public void t4() {
+		
+		/*  Extract all the values for the firstName we are sending 
+		 * 
+		 * */
+		String p1 ="findAll{it.firstName=='";
+		String p2 ="'}.get{0}";
+		
+		HashMap<String,Object> value = SerenityRest.rest().given()
+		.when()
+		.get("/list")
+		.then()
+		.log()
+		.all()
+		.statusCode(200)
+		.extract()    
+		.path(p1+firstName+p2); 
+		
+		System.out.println("value extracted -"+value);
+
+//		assertThat(value, hasvalue(firstName));
+	}
+	
+	@Title("Delete a student and verify if deleted")
+	@Test
+	public void t5() {
+		
+		SerenityRest.rest().given()
+		.when()
+		.delete("/"+"100"); //studentId
+	
+		SerenityRest.rest().given()
+		.when()
+		.get("/"+ "100")
+		.then()
+		.log()
+		.all()
+		.statusCode(404);
+		
+	
+		
+	}
 }
